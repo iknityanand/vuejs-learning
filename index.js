a9b1623a1e242c0071b94b3f3b62356be139c475
iknityanand <iknityanand@gmail.com>
2018-10-07 18:18:08 +0530

    created a list component in Vue.js

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 var card = new Vue({
     el: "#card",
     data: {
-        title: "Dinosaurs",
-        content: "Stone age pets for stone age man "
-    }   
-})
+        title: "Tasks",
+        items: []
+    },
+    methods:{   
+        addItem: () => {
+            var input = document.getElementById('itemForm');
+            if(input.value !== ''){
+                this.card.items.push({ text: input.value})
+                input.value='';
+            }
+        },
+        deleteItem: (index)=>{
+            this.card.items.splice(index, 1);
+        }
+    }  
+});
