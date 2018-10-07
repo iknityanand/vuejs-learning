b3a21b0cc0a4c123161309981794a07d754601e0
iknityanand <iknityanand@gmail.com>
2018-10-07 18:37:18 +0530

    Adding filters in vue.js

--- index.js
+++ index.js
@@ -4,16 +4,28 @@ var card = new Vue({
         title: "Tasks",
         items: []
     },
-    methods:{   
+    methods: {
         addItem: () => {
             var input = document.getElementById('itemForm');
-            if(input.value !== ''){
-                this.card.items.push({ text: input.value})
-                input.value='';
+            if (input.value !== '') {
+                this.card.items.push({ text: input.value })
+                input.value = '';
             }
         },
-        deleteItem: (index)=>{
+        deleteItem: (index) => {
             this.card.items.splice(index, 1);
         }
-    }  
+    },
+    filters: {
+        capitalize: (value) =>{
+            if(!value) return '';
+            value = value.toString();
+            return value.charAt(0).toUpperCase() + value.slice(1);
+        },
+        undercase: (value)=>{
+            if(!value) return '';
+            value = value.toString();
+            return value.charAt(0).toLowerCase() + value.slice(1);
+        }
+    }
 });
